29310bc6309ac585724d68331152bdbf3abc9a09
mr-metschnikowia <gorbunov.lev@googlemail.com>
2022-11-15 09:42:04 +0000

    get comments by id

--- controller.js
+++ controller.js
@@ -1,4 +1,4 @@
-const { selectCategories, selectReviews, selectReview } = require("./model.js");
+const { selectCategories, selectReviews, selectReview, selectComments } = require("./model.js");
 
 exports.getCategories = (req, res) => {
     selectCategories().then(categories => res.status(200).send({ categories: categories }));
@@ -10,5 +10,10 @@ exports.getReviews = (req, res) => {
 
 exports.getReview = (req, res, next) => {
     selectReview(req.params.review_id).then(review => res.status(200).send({ review: review }))
+        .catch(err => next(err));
+};
+
+exports.getComments = (req, res, next) => {
+    selectComments(req.params.review_id).then(comments => res.status(200).send({ comments }))
         .catch(err => next(err));
 };
